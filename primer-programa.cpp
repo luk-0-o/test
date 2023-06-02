183a9f966ce2dd98083e00d0414887b765962b43
Lucas C <134748617+luk-0-o@users.noreply.github.com>
2023-06-02 19:04:20 -0400

    Agregada funcion de nombre ;)

--- primer-programa.cpp
+++ primer-programa.cpp
@@ -1,6 +1,12 @@
-#include<iosteram>
+#include<stdio.h>
 using namespace std;
 
+void nombrar(n) {
+	printf("Hola!, "); printf(n);
+}
 int main() {
-	cout<<"Hola, Mundo! Qué tal?!"<<endl;
+	char nombre[10]="";
+	printf("Hola, Mundo! Qué tal?!\n")
+	printf(%nombre%)
+	nombrar(nombre);
 }
